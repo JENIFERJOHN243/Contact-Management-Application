7fc383f980bb4d36dd750d6935f819a7a4e75d2d
JENIFERJOHN243 <112951358+JENIFERJOHN243@users.noreply.github.com>
2022-10-05 16:09:17 +0530

    Update ContactManagementDAO.java

--- ContactManagementApplication/src/dao/ContactManagementDAO.java
+++ ContactManagementApplication/src/dao/ContactManagementDAO.java
@@ -49,15 +49,39 @@ public class ContactManagementDAO {
 		return userId;
 	}
 
-	public void updateUserDetails(UserDetails user) throws ClassNotFoundException, SQLException {
+	public void updateUserFirstName(UserDetails user) throws ClassNotFoundException, SQLException {
 		getConnection();
-		String sqlQuery = "UPDATE USER_DETAILS SET first_name=?,last_name=?,mob_no=?,mail_id=? WHERE user_id=?";
+		String sqlQuery = "UPDATE USER_DETAILS SET first_name=? WHERE user_id=?";
 		preparedStatement = connection.prepareStatement(sqlQuery);
 		preparedStatement.setString(1, user.getFirstName());
-		preparedStatement.setString(2, user.getLastName());
-		preparedStatement.setString(3, user.getMobileNumber());
-		preparedStatement.setString(4, user.getEmailId());
-		preparedStatement.setInt(5, user.getUserId());
+		preparedStatement.setInt(2, user.getUserId());
+		preparedStatement.executeUpdate();
+	}
+	
+	public void updateUserLastName(UserDetails user) throws ClassNotFoundException, SQLException {
+		getConnection();
+		String sqlQuery = "UPDATE USER_DETAILS SET last_name=? WHERE user_id=?";
+		preparedStatement = connection.prepareStatement(sqlQuery);
+		preparedStatement.setString(1, user.getLastName());
+		preparedStatement.setInt(2, user.getUserId());
+		preparedStatement.executeUpdate();
+	}
+
+	public void updateUserMobileNumber(UserDetails user) throws ClassNotFoundException, SQLException {
+		getConnection();
+		String sqlQuery = "UPDATE USER_DETAILS SET mob_no=? WHERE user_id=?";
+		preparedStatement = connection.prepareStatement(sqlQuery);
+		preparedStatement.setString(1, user.getMobileNumber());
+		preparedStatement.setInt(2, user.getUserId());
+		preparedStatement.executeUpdate();
+	}
+
+	public void updateUserEmailId(UserDetails user) throws ClassNotFoundException, SQLException {
+		getConnection();
+		String sqlQuery = "UPDATE USER_DETAILS SET mail_id=? WHERE user_id=?";
+		preparedStatement = connection.prepareStatement(sqlQuery);
+		preparedStatement.setString(1, user.getEmailId());
+		preparedStatement.setInt(2, user.getUserId());
 		preparedStatement.executeUpdate();
 	}
 
@@ -70,6 +94,44 @@ public class ContactManagementDAO {
 		return result;
 	}
 
+	public ArrayList<UserDetails> searchUsingFirstName(UserDetails user) throws ClassNotFoundException, SQLException {
+		getConnection();
+		ArrayList<UserDetails> searchList = new ArrayList<UserDetails>();
+		String sqlQuery ="SELECT first_name,last_name,mob_no,mail_id FROM USER_DETAILS WHERE first_name=?";
+		preparedStatement = connection.prepareStatement(sqlQuery);
+		preparedStatement.setString(1, user.getFirstName());
+        
+		resultSet = preparedStatement.executeQuery();
+		while(resultSet.next()) {
+			UserDetails user1 = new UserDetails();
+			user1.setFirstName(resultSet.getString(1));
+			user1.setLastName(resultSet.getString(2));
+			user1.setMobileNumber(resultSet.getString(3));
+			user1.setEmailId(resultSet.getString(4));
+			searchList.add(user1);
+		}
+		return searchList;
+	}
+
+	public ArrayList<UserDetails> searchUsingFirstCharacter(String search) throws ClassNotFoundException, SQLException {
+		getConnection();
+		ArrayList<UserDetails> searchValues = new ArrayList<UserDetails>();
+		String sqlQuery ="SELECT first_name,last_name,mob_no,mail_id FROM USER_DETAILS WHERE first_name LIKE ? ORDER BY first_name ASC;";
+		preparedStatement = connection.prepareStatement(sqlQuery);
+		preparedStatement.setString(1, search);
+        
+		resultSet = preparedStatement.executeQuery();
+		while(resultSet.next()) {
+			UserDetails user1 = new UserDetails();
+			user1.setFirstName(resultSet.getString(1));
+			user1.setLastName(resultSet.getString(2));
+			user1.setMobileNumber(resultSet.getString(3));
+			user1.setEmailId(resultSet.getString(4));
+			searchValues.add(user1);
+		}
+		return searchValues;
+	}
+	
 	public ArrayList<UserDetails> viewUserDetails() throws ClassNotFoundException, SQLException {
 		getConnection();
 		ArrayList<UserDetails> userList = new ArrayList<UserDetails>();
@@ -87,22 +149,4 @@ public class ContactManagementDAO {
 		return userList;
 	}
 
-	public ArrayList<UserDetails> searchUserDetails(UserDetails user) throws ClassNotFoundException, SQLException {
-		getConnection();
-		ArrayList<UserDetails> userList = new ArrayList<UserDetails>();
-		String sqlQuery ="SELECT first_name,last_name,mob_no,mail_id FROM USER_DETAILS WHERE first_name=?";
-		preparedStatement = connection.prepareStatement(sqlQuery);
-		preparedStatement.setString(1, user.getFirstName());
-        
-		resultSet = preparedStatement.executeQuery();
-		while(resultSet.next()) {
-			user.setFirstName(resultSet.getString(1));
-			user.setLastName(resultSet.getString(2));
-			user.setMobileNumber(resultSet.getString(3));
-			user.setEmailId(resultSet.getString(4));
-			userList.add(user);
-		}
-		return userList;
-	}
-	
 }
